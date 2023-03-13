39e3bc363c1a870bd58c3ef968230408c9ce0e23
Badreddine-AB <badreddine@gmail.com>
2023-03-13 21:53:23 +0100

    completing the MovieScreen page

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -7,16 +7,25 @@ import {
   Dimensions,
   Image,
   Linking,
-  TouchableOpacity
+  TouchableOpacity,
+  FlatList,
+  Share,
 } from "react-native";
 import Colors from "../constants/Colors";
 import { useEffect, useState } from "react";
-import { getMovieById, getPoster, getVideo } from "../services/MovieService";
+import {
+  getMovieById,
+  getPoster,
+  getVideo,
+  getLanguage,
+} from "../services/MovieService";
 import ItemSeparator from "../components/itemSeperator";
 import { LinearGradient } from "expo-linear-gradient";
 import Fonts from "../constants/Fonts";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import { APPEND_TO_RESPONSE as AR } from "../constants/Urls";
+import CastCard from "../components/CastCard";
+import MovieCard from "../components/MovieCard";
 
 const { height, width } = Dimensions.get("screen");
 
@@ -26,11 +35,13 @@ const setWidth = (w) => (width / 100) * w;
 const MovieScreen = ({ route, navigation }) => {
   const { movieId } = route.params;
   const [movie, setMovie] = useState({});
+  const [isCastSelected, setIsCastSelected] = useState(true);
 
   useEffect(() => {
-    getMovieById(movieId, `${AR.VIDEOS}`).then((response) =>
-      setMovie(response.data)
-    );
+    getMovieById(
+      movieId,
+      `${AR.VIDEOS},${AR.CREDITS},${AR.RECOMMENDATIONS},${AR.SIMILAR}`
+    ).then((response) => setMovie(response?.data));
   }, []);
 
   return (
@@ -45,7 +56,7 @@ const MovieScreen = ({ route, navigation }) => {
         <Image
           style={styles.moviePosterImage}
           resizeMode="cover"
-          source={{ uri: getPoster(movie.backdrop_path) }}
+          source={{ uri: getPoster(movie?.backdrop_path) }}
         />
       </View>
       <View style={styles.headerContainer}>
@@ -55,8 +66,15 @@ const MovieScreen = ({ route, navigation }) => {
         >
           <Feather name="chevron-left" size={35} color={Colors.WHITE} />
         </TouchableOpacity>
-        <Text style={styles.headerText}>Share</Text>
-        
+
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() =>
+            Share.share({ message: `${movie?.title}\n\n${movie?.homepage}` })
+          }
+        >
+          <Text style={styles.headerText}>Share</Text>
+        </TouchableOpacity>
       </View>
       <TouchableOpacity
         style={styles.playButton}
@@ -65,7 +83,116 @@ const MovieScreen = ({ route, navigation }) => {
         <Ionicons name="play-circle-outline" size={70} color={Colors.WHITE} />
       </TouchableOpacity>
       <ItemSeparator height={setHeight(37)} />
-      <Text>{movie.title}</Text>
+      <View style={styles.movieTitleContainer}>
+        <Text style={styles.movieTitle} numberOfLines={2}>
+          {movie?.original_title}
+        </Text>
+        <View style={styles.row}>
+          <Ionicons name="heart" size={22} color={Colors.HEART} />
+          <Text style={styles.ratingText}>{movie?.vote_average}</Text>
+        </View>
+      </View>
+      <Text style={styles.genreText}>
+        {movie?.genres?.map((genre) => genre?.name)?.join(", ")} |{" "}
+        {movie?.runtime} Min
+      </Text>
+      <Text style={styles.genreText}>
+        {getLanguage(movie?.original_language)?.english_name}
+      </Text>
+      <View style={styles.overviewContainer}>
+        <Text style={styles.overviewTitle}>OverView</Text>
+        <Text style={styles.overviewText}>{movie?.overview}</Text>
+      </View>
+      <View>
+        <Text style={styles.castTitle}>Cast</Text>
+        <View style={styles.castSubMenuContainer}>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => setIsCastSelected(true)}
+          >
+            <Text
+              style={{
+                ...styles.castSubMenuText,
+                color: isCastSelected ? Colors.BLACK : Colors.LIGHT_GRAY,
+              }}
+            >
+              Cast
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => setIsCastSelected(false)}
+          >
+            <Text
+              style={{
+                ...styles.castSubMenuText,
+                color: isCastSelected ? Colors.LIGHT_GRAY : Colors.BLACK,
+              }}
+            >
+              Crew
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          style={{ marginVertical: 5 }}
+          data={isCastSelected ? movie?.credits?.cast : movie?.credits?.crew}
+          keyExtractor={(item) => item?.credit_id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          ListHeaderComponent={() => <ItemSeparator width={20} />}
+          ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+          ListFooterComponent={() => <ItemSeparator width={20} />}
+          renderItem={({ item }) => (
+            <CastCard
+              originalName={item?.name}
+              characterName={isCastSelected ? item?.character : item?.job}
+              image={item?.profile_path}
+            />
+          )}
+        />
+      </View>
+      <Text style={styles.extraListTitle}>Recommended Movies</Text>
+      <FlatList
+        data={movie?.recommendations?.results}
+        keyExtractor={(item) => item?.id?.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ListHeaderComponent={() => <ItemSeparator width={20} />}
+        ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+        ListFooterComponent={() => <ItemSeparator width={20} />}
+        renderItem={({ item }) => (
+          <MovieCard
+            title={item.title}
+            language={item.original_language}
+            voteAverage={item.vote_average}
+            voteCount={item.vote_count}
+            poster={item.poster_path}
+            size={0.6}
+            onPress={() => navigation.navigate("movie", { movieId: item.id })}
+          />
+        )}
+      />
+      <Text style={styles.extraListTitle}>Similar Movies</Text>
+      <FlatList
+        data={movie?.similar?.results}
+        keyExtractor={(item) => item?.id?.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ListHeaderComponent={() => <ItemSeparator width={20} />}
+        ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+        ListFooterComponent={() => <ItemSeparator width={20} />}
+        renderItem={({ item }) => (
+          <MovieCard
+            title={item.title}
+            language={item.original_language}
+            voteAverage={item.vote_average}
+            voteCount={item.vote_count}
+            poster={item.poster_path}
+            size={0.6}
+            onPress={() => navigation.navigate("movie", { movieId: item.id })}
+          />
+        )}
+      />
     </ScrollView>
   );
 };
@@ -118,6 +245,77 @@ const styles = StyleSheet.create({
     top: 110,
     left: setWidth(50) - 70 / 2,
     elevation: 10,
+  },
+  movieTitleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  movieTitle: {
+    color: Colors.BLACK,
+    fontFamily: Fonts.EXTRA_BOLD,
+    fontSize: 18,
+    width: setWidth(60),
+  },
+  ratingText: {
+    marginLeft: 5,
+    color: Colors.BLACK,
+    fontFamily: Fonts.EXTRA_BOLD,
+    fontSize: 15,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  genreText: {
+    color: Colors.LIGHT_GRAY,
+    paddingHorizontal: 20,
+    paddingTop: 5,
+    fontFamily: Fonts.BOLD,
+    fontSize: 13,
+  },
+  overviewContainer: {
+    backgroundColor: Colors.EXTRA_LIGHT_GRAY,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+  },
+  overviewTitle: {
+    color: Colors.BLACK,
+    fontFamily: Fonts.BOLD,
+    fontSize: 18,
+  },
+  overviewText: {
+    color: Colors.LIGHT_GRAY,
+    paddingVertical: 5,
+    fontFamily: Fonts.BOLD,
+    fontSize: 13,
+    textAlign: "justify",
+  },
+  castTitle: {
+    marginLeft: 20,
+    color: Colors.BLACK,
+    fontFamily: Fonts.BOLD,
+    fontSize: 18,
+  },
+  castSubMenuContainer: {
+    marginLeft: 20,
+    flexDirection: "row",
+    marginVertical: 5,
+  },
+  castSubMenuText: {
+    marginRight: 10,
+    color: Colors.BLACK,
+    fontFamily: Fonts.BOLD,
+    fontSize: 13,
+  },
+  extraListTitle: {
+    marginLeft: 20,
+    color: Colors.BLACK,
+    fontFamily: Fonts.BOLD,
+    fontSize: 18,
+    marginVertical: 8,
   },
 });
 
